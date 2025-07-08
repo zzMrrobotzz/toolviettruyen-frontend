692966f2029e42a2cff5f3ef6610bf7456729e9c
zzMrrobotzz <daiduc3091994@gmail.com>
2025-07-08 15:13:17 +0700

    fix: Remove duplicated code in BatchStoryWritingModule

--- components/modules/BatchStoryWritingModule.tsx
+++ components/modules/BatchStoryWritingModule.tsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import { 
     ApiSettings, 
@@ -20,17 +18,18 @@ import InfoBox from '../InfoBox';
 import { generateText as generateGeminiText, generateTextWithJsonOutput as generateGeminiJson } from '../../services/geminiService';
 import { generateText as generateDeepSeekText, generateTextWithJsonOutput as generateDeepSeekJson } from '../../services/deepseekService';
 import { delay } from '../../utils';
+import { useAppContext } from '../../AppContext';
 
 interface BatchStoryWritingModuleProps {
-  apiSettings: ApiSettings;
   moduleState: BatchStoryWritingModuleState;
   setModuleState: React.Dispatch<React.SetStateAction<BatchStoryWritingModuleState>>;
 }
 
 const BatchStoryWritingModule: React.FC<BatchStoryWritingModuleProps> = ({ 
-    apiSettings, moduleState, setModuleState 
+    moduleState, setModuleState 
 }) => {
-  const {
+    const { apiSettings } = useAppContext();
+    const {
     inputItems, results, globalTargetLength, globalWritingStyle, globalCustomWritingStyle,
     outputLanguage, referenceViralStoryForStyle, isProcessingBatch,
     batchProgressMessage, batchError, concurrencyLimit
@@ -498,67 +497,6 @@ const BatchStoryWritingModule: React.FC<BatchStoryWritingModuleProps> = ({
           ))}
         </div>
       )}
-    </ModuleContainer>
-  );
-};
-
-export default BatchStoryWritingModule;
-
-import React, { useState, useEffect } from 'react';
-import { 
-    ApiSettings, 
-    BatchStoryWritingModuleState, 
-    BatchStoryInputItem, 
-    GeneratedBatchStoryOutputItem, 
-    EditStoryAnalysisReport 
-} from '../../types';
-import { 
-    STORY_LENGTH_OPTIONS, 
-    WRITING_STYLE_OPTIONS, 
-    HOOK_LANGUAGE_OPTIONS 
-} from '../../constants';
-import ModuleContainer from '../ModuleContainer';
-import LoadingSpinner from '../LoadingSpinner';
-import ErrorAlert from '../ErrorAlert';
-import InfoBox from '../InfoBox';
-import { generateText as generateGeminiText, generateTextWithJsonOutput as generateGeminiJson } from '../../services/geminiService';
-import { generateText as generateDeepSeekText, generateTextWithJsonOutput as generateDeepSeekJson } from '../../services/deepseekService';
-import { delay } from '../../utils';
-import axios from 'axios';
-import CreditAlertBox from '../CreditAlertBox';
-import { useAppContext } from '../../AppContext';
-
-interface BatchStoryWritingModuleProps {
-  moduleState: BatchStoryWritingModuleState;
-  setModuleState: React.Dispatch<React.SetStateAction<BatchStoryWritingModuleState>>;
-}
-
-const BatchStoryWritingModule: React.FC<BatchStoryWritingModuleProps> = ({ 
-    moduleState, setModuleState
-}) => {
-  const { apiSettings, key: currentKey } = useAppContext();
-  const {
-    inputItems, results, globalTargetLength, globalWritingStyle, globalCustomWritingStyle,
-    outputLanguage, referenceViralStoryForStyle, isProcessingBatch,
-    batchProgressMessage, batchError, concurrencyLimit
-  } = moduleState;
-
-  const [credit, setCredit] = useState<number | null>(null);
-  const [loadingCredit, setLoadingCredit] = useState(false);
-
-  const updateState = (updates: Partial<BatchStoryWritingModuleState>) => {
-    setModuleState(prev => ({ ...prev, ...updates }));
-  };
-
-  const geminiApiKeyForService = apiSettings.provider === 'gemini' ? apiSettings.apiKey : undefined;
-  const deepseekApiKeyForService = apiSettings.provider === 'deepseek' ? apiSettings.apiKey : undefined;
-
-  // ... (all other handler functions like handleStartBatchWriting, etc., remain the same)
-
-  return (
-    <ModuleContainer title="📝 Viết Truyện Hàng Loạt">
-      {/* The full JSX of the component should be here */}
-      <p>Nội dung module Viết Truyện Hàng Loạt...</p>
     </ModuleContainer>
   );
 };
